03e396c1dbadfd8f9b06556432c6ed26e84a1c74
cy_arunya_jayan <arunya.j@cycloides.co.in>
2020-08-14 21:26:57 +0530

    IN-97 model migration for student table (#1)

--- models/student.py
+++ models/student.py
@@ -4,6 +4,6 @@ from settings import db
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique = True)
+    username = db.Column(db.String(100), unique = True)
     password = db.Column(db.String)
     verified = db.Column(db.Boolean)
--- settings.py
+++ settings.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5433/inscholarisdev'
+
+### Environment variables from .env file
+
+db_username=os.environ.get('DB_USERNAME')
+db_password=os.environ.get('DB_PASSWORD')
+host=os.environ.get('HOST')
+port=os.environ.get('PORT')
+db_name=os.environ.get('DB_NAME')
+
+DB_CONNECTION_STRING = 'postgresql://'+str(db_username)+':'+str(db_password)+'@'+str(host)+':'+str(port)+'/'+str(db_name)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION_STRING
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
