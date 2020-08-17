e32d6cd9f9061b40fe4264c3d192ad764474e229
cy_arunya_jayan <arunya.j@cycloides.co.in>
2020-08-17 21:31:13 +0530

    Mail server setup  and template creation

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
-from settings import app
+from settings import app,mail
 from apis import api
 from models import *
-
+from flask_mail import Mail, Message
+from flask import Flask,request,jsonify,render_template
 
 # app = Flask(__name__)
 api.init_app(app)
+
+@app.route('/send_mail',methods=['GET','POST'])
+def send_mail():
+    msg = Message("Welcome to InScholaris", recipients=['arunyajayan96@gmail.com'])
+    msg.html = render_template('verification_mail.html',sending_mail=True)
+    mail.send(msg)
+    return jsonify({"message": "Mail sent successfully"})
+    # msg = Message("Welcome to InScholaris", recipients=['arunyajayan96@gmail.com'])
+    # msg.body = "You have received a welcome mail from InScholaris"
+    # mail.send(msg)
+    # return jsonify({"message":"Mail sent successfully"})
+
 
 if __name__ == '__main__':
     app.run(debug=False)
--- settings.py
+++ settings.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import os
-app = Flask(__name__)
+from flask_mail import Mail, Message
+
+app = Flask(__name__, template_folder="templates")
 
 ### Environment variables from .env file
 
@@ -13,7 +15,17 @@ port=os.environ.get('PORT')
 db_name=os.environ.get('DB_NAME')
 
 DB_CONNECTION_STRING = 'postgresql://'+str(db_username)+':'+str(db_password)+'@'+str(host)+':'+str(port)+'/'+str(db_name)
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'arunyajayan@gmail.com'  # enter your email here
+app.config['MAIL_DEFAULT_SENDER'] = 'arunyajayan@gmail.com' # enter your email here
+app.config['MAIL_PASSWORD'] = 'arunyajayan123#' # mail password
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION_STRING
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+mail = Mail(app)
