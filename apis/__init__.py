8494c3afe31bd6e12432f77772905c72a33a3cd2
ajeesh.anil@cycloides.co.in <ajeesh.anil@cycloides.co.in>
2020-08-25 23:00:12 +0530

    IN-5 mobile onboard images supplying API
    Signed-off-by: ajeesh.anil@cycloides.co.in <ajeesh.anil@cycloides.co.in>

--- apis/__init__.py
+++ apis/__init__.py
@@ -3,6 +3,7 @@ from .Cat import api as ns1
 from .student_api import Signup,VerifyStudent
 from .authentication_apis import Login,RefreshTtoken
 from .enumeration_apis import ListCountries,ListStates,ListCountryStates,ListCities,ListStatesCities,ListLanguages
+from .mobile_specific_api import MobileOnboardImage
 
 api = Api(
     title='InScholaris API',
---
+++ apis/mobile_specific_api.py
@@ -0,0 +1,34 @@
+from flask_jwt_extended import jwt_required
+from flask_restplus import Resource
+
+from core import success
+from settings import app
+
+
+def select_onboard_images ( ) :
+	return [
+		{
+			'id':1,
+			"image":app.config['BACK_END_URL']+'static/mobile_onboard_image_1.png',
+			"description":"""One platform, Many countries, Every University"""
+		},
+		{
+			'id':2,
+			"image" : app.config [ 'BACK_END_URL' ] + 'static/mobile_onboard_image_2.png',
+			"description":"""Create You profile
+Create your InScholaris account, You could attach the digital copies of your educational transcripts, IELTS Score Card etc to your profile/account"""
+		},
+		{
+			'id':3,
+			"image" : app.config [ 'BACK_END_URL' ] + 'static/mobile_onboard_image_3.png',
+			"description":"""InScholar
+InScholaris' conversational AI Chatbots and framework help you to compare and find the right programs & colleges matched with your profile, academics and interests"""
+		}
+	]
+
+
+
+class MobileOnboardImage(Resource):
+    @jwt_required
+    def get(self):
+        return success(data=select_onboard_images(),message='List of onbaording images')
